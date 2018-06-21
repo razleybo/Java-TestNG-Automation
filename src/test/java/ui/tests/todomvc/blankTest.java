6ed89bf8c835d0d0f3c7e734092a03aeca812ba0
Raz <razleybo@gmail.com>
2018-06-21 16:52:28 +0300

    add project and configurations

---
+++ src/test/java/ui/tests/todomvc/blankTest.java
@@ -0,0 +1,12 @@
+package ui.tests.todomvc;
+
+import org.testng.annotations.Test;
+
+
+public class blankTest {
+    @Test
+    public void thisIsMaBlankTest()
+    {
+        System.out.println("my megafucking awesome Test");
+    }
+}
