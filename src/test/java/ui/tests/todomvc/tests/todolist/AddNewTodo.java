7aeb57ded8b3ad96158e8f38e712f3250ad1f11b
Raz <razleybo@gmail.com>
2018-06-24 18:45:27 +0300

    start adding allure

--- src/test/java/ui/tests/todomvc/tests/todolist/AddNewTodo.java
+++ src/test/java/ui/tests/todomvc/tests/todolist/AddNewTodo.java
@@ -1,19 +1,26 @@
 package ui.tests.todomvc.tests.todolist;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import ui.tests.todomvc.tests.BaseTest;
 
+
 public class AddNewTodo extends BaseTest {
-   private String toDoName = "My Awesome ToDo";
+    final int MAX_TASKS = 10 ;
+    private String toDoName = "My Awesome ToDo";
+
+    @Description("This test create a new Todo task and verify it location on the list")
     @Test
+    @Step("Create first task")
     public void addNewTask(){
         todoListActions.goToTodoListPage();
         todoListActions.addNewTodo(toDoName);
         todoListActions.verifyLastTodo(toDoName);
     }
     @Test(dependsOnMethods = "addNewTask")
+    @Step("Verify new tasks are added below")
     public void addMultipleTasks(){
-        final int MAX_TASKS = 10 ;
         for (int todoIteration =0 ;todoIteration<MAX_TASKS ; todoIteration++) {
             todoListActions.addNewTodo(toDoName+todoIteration);
             todoListActions.verifyLastTodo(toDoName+todoIteration);
